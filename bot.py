e8c83840901831a24ca89e5126a8d00c99d3995e
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-15 15:17:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,10 @@ app = Client('my_account')
 
 
 
-@app.on_message(Filters.chat(-1001353340635) & Filters.text)
+@app.on_message(Filters.chat(-1001129914210) & Filters.text)
 def forward(client, message):
-          client.forward_messages(-1001344956857, -1001353340635, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-          client.forward_messages(-1001356076506, -1001353340635, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+          client.forward_messages(-1001344956857, -1001129914210, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+          client.forward_messages(-1001356076506, -1001129914210, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
     
 
 app.run()
