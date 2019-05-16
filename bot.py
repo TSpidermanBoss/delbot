1883a3ba74d2b1ff2d0f74b41527eff26677adcd
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-16 08:58:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,7 @@ from pyrogram import Client, Filters
 app = Client('my_account')
 
 
-
-@app.on_message(Filters.chat(-1001278232224 & Filters.text)
+@app.on_message(Filters.chat(-1001278232224) & Filters.text)
 def forward(client, message):
           client.forward_messages(-1001424923281, -1001129914210, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
           
