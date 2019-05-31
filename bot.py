7c98cfea95069c3646a3456577aa1c8b36a1a6b3
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-31 15:08:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,85 +1,58 @@
 from pyrogram import Client, Filters
-app = Client('my_account')
-@app.on_message(Filters.chat(-1001378725482) & Filters.text)
+app = Client('890398475:AAFKe-e5H2owXvzoB-AlIpy3ELE-0f7OoNs')
+@app.on_message(Filters.chat(-1001369162545) & Filters.text)
 def forawrd(client, message):
-    files = open("sure.txt" , "r")
     file = open("text.txt" , "r")
     lines = file.readlines()
-    liner = files.readlines()
     file.close()
-    files.close()
-    
     for line in lines:
-      if line == "started": 
-          client.forward_messages(-1001344956857,-1001378725482, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-          client.forward_messages(-1001356076506,-1001378725482, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-    for line in liner:
-      if line == "started":
-            filed = open("sue.txt", "r")
-            lined = filed.readlines()
-            filed.close()
-            for line in lined:
-                 client.forward_messages(-1001129066774,-1001378725482, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
-                 client.forward_messages(-1001356076506,-1001378725482, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
+      if not line == "closed":
+         client.forward_messages(-1001297100007,-1001369162545, [message.message_id], 'bool = None ', 'bool = True' , 'bool = True' )
 @app.on_message(Filters.command('status'))
 def main(client, message) :
-  if message.from_user.id == 491634139:
+    if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
       file = open("text.txt" , "r")
       lines = file.readlines()
       file.close()
       for line in lines:
         if line == "started":
-           message.reply("Forwarding is on ! ")
+           message.reply("line is on ! ")
         if line == "closed":
-           message.reply("Forwarding is stopped ! ")
- 
-
+           message.reply("line is stopped ! ")
+    if client.get_chat_member(message.chat.id , message.from_user.id).status == 'creator':
+      file = open("text.txt" , "r")
+      lines = file.readlines()
+      file.close()
+      for line in lines:
+        if line == "started":
+           message.reply("line is on ! ")
+        if line == "closed":
+           message.reply("line is stopped ! ")        
 @app.on_message(Filters. private)
 def ran( client, message) :
   client.forward_messages(-1001250871922, message.chat.id ,[message.message_id])
 @app.on_message(Filters.command('offline'))
 def main(client, message) :
- 
+   if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     file = open("text.txt" , "w")
     file.write("closed")
     file.close()
-    message.reply("Forwarding is off ! ")
+    message.reply("line is off ! ")
+   if client.get_chat_member(message.chat.id , message.from_user.id).status == 'creator':
+    file = open("text.txt" , "w")
+    file.write("closed")
+    file.close()
+    message.reply("line is off ! ")
 @app.on_message(Filters.command('online'))
 def main(client, message) :
-  if message.from_user.id == 491634139:
- 
+   if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     file = open("text.txt" , "w")
     file.write("started")
     file.close()
-    message.reply("Forwarding is on !")
-
-@app.on_message(Filters.command('zearnon'))
-def main(client, message) :
-  if message.from_user.id == 491634139:
- 
-    files = open("sure.txt" , "w")
-    files.write("started")
-    files.close()
-    message.reply("Forwarding is on(zearn)!")
-
-
-@app.on_message(Filters.command('zearnoff'))
-def main(client, message) :
-  if message.from_user.id == 491634139:
- 
-    files = open("sure.txt" , "w")
-    files.write("closed")
-    files.close()
-    message.reply("Forwarding is off(zearn)!")
-
-
-
-app.on_message(Filters. private)
-def main(client, message) :
-    files = open("sue.txt" , "w")
-    files.write(message)
-    files.close()
-    message.reply(message)
-
-
+    message.reply("line is on !")
+   if client.get_chat_member(message.chat.id , message.from_user.id).status == 'creator':
+    file = open("text.txt" , "w")
+    file.write("started")
+    file.close()
+    message.reply("line is on !")
 app.run()
