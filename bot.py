b2eac1e3e050d1649b48d4b757a7b2e9fb65d66e
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-09 10:03:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 
 from pyrogram import Client, Filters,Emoji
-app = Client('878426361:AAEv5nyWvDwikohxrGxS6mCLpgeyPVjHKTQ',605563,"7f2c2d12880400b88764b9b304e14e0b")
+app = Client('666639160:AAEtopjBMU5r_i_UROx8PTWSSOtIrU6V7W8',605563,"7f2c2d12880400b88764b9b304e14e0b")
 
 
 
-u = '-1001202080858'
+u = '-1001336546427'
 
 s = '-1001378725482'
 
