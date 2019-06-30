56175dbed1046932980d85016cfd8bed8995b3a4
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-30 23:03:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 
 from pyrogram import Client, Filters,Emoji
-app = Client('666639160:AAEtopjBMU5r_i_UROx8PTWSSOtIrU6V7W8',605563,"7f2c2d12880400b88764b9b304e14e0b")
+app = Client("870127745:AAGKhzPQ__N3pn56_44Sn86yGOLKwvbu-0k",715451,"d2cba6f7bf5d1a45682da5bb9071a307")
 
 @app.on_message(Filters.command("send"))
 def forawrd(client, message):
