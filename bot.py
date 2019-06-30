3283b0f53fbde3b049e03070cbc8a079c60d9de4
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-30 23:00:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,6 @@
 from pyrogram import Client, Filters,Emoji
 app = Client('666639160:AAEtopjBMU5r_i_UROx8PTWSSOtIrU6V7W8',605563,"7f2c2d12880400b88764b9b304e14e0b")
 
-
-
-u = '-1001336546427'
-
-s = '-1001378725482'
-
 @app.on_message(Filters.command("send"))
 def forawrd(client, message):
  files = open("sure.txt","r")
