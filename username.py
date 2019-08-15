d5f0b8a7786ba6662d971d104b8d9de6e7b83781
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-15 12:32:35 +0530

    Update username.py

--- username.py
+++ username.py
@@ -1,5 +1,13 @@
 from pyrogram import Client, Filters,Emoji
 app = Client("session",bot_token="622131522:AAFUneglCPEgaZKa8lcaYCnIKitXjct8YGU",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
+
+@app.on_message(Filters. private & Filters.command("start"))
+def ran( client, message) :
+  message.reply( """♻️ This is antiusername created by a wonderful [person](https://t.me/Google_console) ✍️.
+
+Add me to your group i will delete username from non admins automatically.""")
+
+
 @app.on_message(Filters.text)
 def main(client, message):
  b = client.get_chat_member(message.chat.id , message.from_user.id).status
@@ -7,11 +15,6 @@ def main(client, message):
   if "@" in message.text:
    client.delete_messages(message.chat.id, message.message_id)
 
-@app.on_message(Filters. private & Filters.command("start"))
-def ran( client, message) :
-  message.reply( """♻️ This is antiusername created by a wonderful [person](https://t.me/Google_console) ✍️.
-
-Add me to your group i will delete username from non admins automatically.""")
 
 
 app.run()
