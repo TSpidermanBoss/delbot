8a579bc5fad635c6b48f28c303995c23130b9261
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-15 12:30:16 +0530

    Update username.py

--- username.py
+++ username.py
@@ -1,8 +1,5 @@
 from pyrogram import Client, Filters,Emoji
-app = Client("session",bot_token="903164012:AAH6f9qw6TGaF7DJkiaCeqjp7oaM92p93fc",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
-
-#("session",bot_token="622131522:AAFUneglCPEgaZKa8lcaYCnIKitXjct8YGU",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b")
-
+app = Client("session",bot_token="622131522:AAFUneglCPEgaZKa8lcaYCnIKitXjct8YGU",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
 @app.on_message(Filters.text)
 def main(client, message):
  b = client.get_chat_member(message.chat.id , message.from_user.id).status
