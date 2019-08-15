2f9d07dfad28f6e02ba9c28a0024006eb6891ce7
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-15 14:06:57 +0530

    Update username.py

--- username.py
+++ username.py
@@ -1,4 +1,6 @@
 from pyrogram import Client, Filters,Emoji
+import re
+
 app = Client("session",bot_token="622131522:AAFUneglCPEgaZKa8lcaYCnIKitXjct8YGU",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
 
 @app.on_message(Filters. private & Filters.command("start"))
@@ -12,8 +14,10 @@ Add me to your group i will delete username from non admins automatically.""")
 def main(client, message):
  b = client.get_chat_member(message.chat.id , message.from_user.id).status
  if not b == 'administrator' or b == "creator":  
-  if "@" in message.text:
-   client.delete_messages(message.chat.id, message.message_id)
+   txt = message.text
+   x = re.search("^@", txt)
+   print(x)
+   #client.delete_messages(message.chat.id, message.message_id)
 
 
 
