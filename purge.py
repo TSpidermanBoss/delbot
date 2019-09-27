59866ebcd6eac1cb09db6aad41efdee6cde7b7b5
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-09-27 22:44:21 +0530

    Update purge.py

--- purge.py
+++ purge.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, Filters,Emoji
-app = Client("session",bot_token="912058665:AAEoMfZrqGnfd6OJr32sC5e4CEKEpxEVz3w",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
+app = Client("session",bot_token="745460463:AAGN6LBCTNI-9ZXz5eQF9ouP0M8p3rzyZAM",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
 
-@app.on_message(Filters.command("send"))
+@app.on_message(Filters.command("purge"))
 def forawrd(client, message):
  x = client.get_chat_member(message.chat.id , message.from_user.id)
  if x.status == "administrator" or x.status == "creator":
