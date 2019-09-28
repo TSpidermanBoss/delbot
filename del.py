16b5855255217e60045bd8e81f9678be15715fd9
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-09-28 22:37:36 +0530

    Update del.py

--- del.py
+++ del.py
@@ -1,45 +1,18 @@
-from pyrogram import Client, Filters,Emoji
-app = Client("my_account",800295,"3d6ffe66b0d34f2921b964da418d9931")
+from pyrogram import Client, Filters
+import time
+from pyrogram.errors import FloodWait
+app = Client (session_name=input("Enter session name : ") ,api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9",bot_token = input("Enter bot token: "))                                   
 
 @app.on_message(Filters.command("e"))
 def main(client, message):
- for message.message_id in range(message.command[1],message.command[2]):
+x = message.command[1]
+y = message.command[2]
+print(x)
+print(y)
+print(int(x))
+for m in range(int(x),int(y)):
   try:
-   client.delete_messages(message.chat.id,message.message_id)
+   client.delete_messages(message.chat.id,m)
   except:
    continue
-
-@app.on_message(Filters.command("a"))
-def main(client, message):
- for message.message_id in range(message.command[1],message.command[2]):
-  try:
-   client.delete_messages(message.chat.id,message.message_id)
-  except:
-   continue
-
-@app.on_message(Filters.command("b"))
-def main(client, message):
- for message.message_id in range(message.command[1],message.command[2]):
-  try:
-   client.delete_messages(message.chat.id,message.message_id)
-  except:
-   continue
-
-@app.on_message(Filters.command("c"))
-def main(client, message):
- for message.message_id in range(message.command[1],message.command[2]):
-  try:
-   client.delete_messages(message.chat.id,message.message_id)
-  except:
-   continue
-
-@app.on_message(Filters.command("d"))
-def main(client, message):
- for message.message_id in range(message.command[1],message.command[2]):
-  try:
-   client.delete_messages(message.chat.id,message.message_id)
-  except:
-   continue
-
-
 app.run()
