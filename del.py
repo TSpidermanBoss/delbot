2647760a294f626f5f031d2d4f4bf7b61fa86448
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-09-28 21:15:42 +0530

    Update del.py

--- del.py
+++ del.py
@@ -3,19 +3,15 @@ app = Client ("session",771202,"28eed966b0cd4238a4f4f8f0ab4c9c72")
 
 @app.on_message(Filters.command("e"))
 def forawrd(client, message):
- x = client.get_chat_member(message.chat.id , message.from_user.id)
- if x.status == "administrator" or x.status == "creator":
-  x = int(message.text.split(' ')[1])
-  y = int(message.text.split(' ')[2])
-  for m in range(x,y):
-   try:
-    client.delete_messages(message.chat.id,m)
-   except:
-    continue
+ x = int(message.text.split(' ')[1])
+ y = int(message.text.split(' ')[2])
+ for m in range(x,y):
+  try:
+   client.delete_messages(message.chat.id,m)
+  except:
+   continue
 @app.on_message(Filters.command("a"))
 def forawrd(client, message):
- x = client.get_chat_member(message.chat.id , message.from_user.id)
- if x.status == "administrator" or x.status == "creator":
   x = int(message.text.split(' ')[1])
   y = int(message.text.split(' ')[2])
   for m in range(x,y):
@@ -25,8 +21,6 @@ def forawrd(client, message):
     continue
 @app.on_message(Filters.command("b"))
 def forawrd(client, message):
- x = client.get_chat_member(message.chat.id , message.from_user.id)
- if x.status == "administrator" or x.status == "creator":
   x = int(message.text.split(' ')[1])
   y = int(message.text.split(' ')[2])
   for m in range(x,y):
@@ -36,8 +30,6 @@ def forawrd(client, message):
     continue
 @app.on_message(Filters.command("c"))
 def forawrd(client, message):
- x = client.get_chat_member(message.chat.id , message.from_user.id)
- if x.status == "administrator" or x.status == "creator":
   x = int(message.text.split(' ')[1])
   y = int(message.text.split(' ')[2])
   for m in range(x,y):
@@ -47,8 +39,6 @@ def forawrd(client, message):
     continue
 @app.on_message(Filters.command("d"))
 def forawrd(client, message):
- x = client.get_chat_member(message.chat.id , message.from_user.id)
- if x.status == "administrator" or x.status == "creator":
   x = int(message.text.split(' ')[1])
   y = int(message.text.split(' ')[2])
   for m in range(x,y):
