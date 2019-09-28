05ca1808c74ea2b6eb51a91eb846f169fd09d085
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-09-28 22:26:47 +0530

    Update del.py

--- del.py
+++ del.py
@@ -3,7 +3,7 @@ app = Client("my_account",800295,"3d6ffe66b0d34f2921b964da418d9931")
 
 @app.on_message(Filters.command("e"))
 def main(client, message):
- for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
+ for message.message_id in range(int(message.command[1]), int(message.command[2])):
   try:
    client.delete_messages(message.chat.id,message.message_id)
   except:
@@ -11,31 +11,35 @@ def main(client, message):
 
 @app.on_message(Filters.command("a"))
 def main(client, message):
- for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
+ for message.message_id in range(int(message.command[1]), int(message.command[2])):
   try:
    client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
+
 @app.on_message(Filters.command("b"))
 def main(client, message):
- for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
+ for message.message_id in range(int(message.command[1]), int(message.command[2])):
   try:
    client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
+
 @app.on_message(Filters.command("c"))
 def main(client, message):
- for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
+ for message.message_id in range(int(message.command[1]), int(message.command[2])):
   try:
    client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
+
 @app.on_message(Filters.command("d"))
 def main(client, message):
- for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
+ for message.message_id in range(int(message.command[1]), int(message.command[2])):
   try:
    client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
+
 
 app.run()
