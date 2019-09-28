f38c9143a6f45cadef54836ed1f230e270456698
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-09-28 21:56:27 +0530

    Update del.py

--- del.py
+++ del.py
@@ -2,51 +2,39 @@ from pyrogram import Client, Filters,Emoji
 app = Client ("session",771202,"28eed966b0cd4238a4f4f8f0ab4c9c72")
 
 @app.on_message(Filters.command("e"))
-def forawrd(client, message):
- x = message.text.split(' ')[1]
- y = message.text.split(' ')[2]
- for m in range(int(x),int(y)):
+def main(client, message):
+ for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
   try:
-   client.delete_messages(message.chat.id,m)
+   client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
-from pyrogram import Client, Filters,Emoji
-app = Client ("session",771202,"28eed966b0cd4238a4f4f8f0ab4c9c72")
 
 @app.on_message(Filters.command("a"))
-def forawrd(client, message):
- x = message.text.split(' ')[1]
- y = message.text.split(' ')[2]
- for m in range(int(x),int(y)):
+def main(client, message):
+ for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
   try:
-   client.delete_messages(message.chat.id,m)
+   client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
-@app.on_message(Filters.command("d"))
-def forawrd(client, message):
- x = message.text.split(' ')[1]
- y = message.text.split(' ')[2]
- for m in range(int(x),int(y)):
+@app.on_message(Filters.command("b"))
+def main(client, message):
+ for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
   try:
-   client.delete_messages(message.chat.id,m)
+   client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
 @app.on_message(Filters.command("c"))
-def forawrd(client, message):
- x = message.text.split(' ')[1]
- y = message.text.split(' ')[2]
- for m in range(int(x),int(y)):
+def main(client, message):
+ for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
   try:
-   client.delete_messages(message.chat.id,m)
+   client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
-@app.on_message(Filters.command("f"))
-def forawrd(client, message):
- x = message.text.split(' ')[1]
- y = message.text.split(' ')[2]
- for m in range(int(x),int(y)):
+@app.on_message(Filters.command("d"))
+def main(client, message):
+ for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
   try:
-   client.delete_messages(message.chat.id,m)
+   client.delete_messages(message.chat.id,message.message_id)
   except:
    continue
 
