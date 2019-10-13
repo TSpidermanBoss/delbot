e2c53913d7c0695cf20cc34f288fa14001c9c472
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-13 10:13:18 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, Filters
 from pyrogram.errors import FloodWait
 import time
-import math
 import pynewtonmath as newton
 
 app = Client("mnnn",bot_token="836655322:AAGlcw1XO_I7ZJqs9iVzQE8RgMbfCcHWxk0",api_id=768402,api_hash="f6420bf67303614279049d48d3e670f6")
