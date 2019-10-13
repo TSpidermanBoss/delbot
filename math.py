28eff95c5cef063bdb8442903b290c1a5108f4a4
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-13 10:28:03 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,13 +1,11 @@
+import pynewtonmath as newton
 from pyrogram import Client, Filters
 from pyrogram.errors import FloodWait
 import time
-import pynewtonmath as newton
-
 app = Client("mnnn",bot_token="836655322:AAGlcw1XO_I7ZJqs9iVzQE8RgMbfCcHWxk0",api_id=768402,api_hash="f6420bf67303614279049d48d3e670f6")
-
 @app.on_message(Filters.command("derive"))
 def main(client, message):
  x = message.text.split(' ')[1]
- message.reply(newton.derive(x) + "thats okk")
+ client.send_message(message.chat.id,newton.derive(x) + "thats okk")
 
 app.run()
