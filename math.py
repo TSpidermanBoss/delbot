a726a62aa710e20ef418c4b1f94928354e4e866d
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-13 10:22:10 +0530

    Update math.py

--- math.py
+++ math.py
@@ -5,9 +5,9 @@ import pynewtonmath as newton
 
 app = Client("mnnn",bot_token="836655322:AAGlcw1XO_I7ZJqs9iVzQE8RgMbfCcHWxk0",api_id=768402,api_hash="f6420bf67303614279049d48d3e670f6")
 
-@app.on_message(Filters.private & Filters.command("derive"))
+@app.on_message(Filters.command("derive"))
 def main(client, message):
  x = message.text.split(' ')[1]
- message.reply(newton.derive(x))
+ message.reply(newton.derive(x) + "thats okk")
 
 app.run()
