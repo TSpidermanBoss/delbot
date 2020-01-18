464fc1f8ee8daaf181f96c48a1949d0eca353c3a
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2020-01-18 10:58:20 +0530

    Update del.py

--- del.py
+++ del.py
@@ -2,7 +2,7 @@ from pyrogram import Client, Filters
 import time
 from pyrogram.errors import FloodWait
 app = Client("my_account",800295,"3d6ffe66b0d34f2921b964da418d9931")
-@app.on_message(Filters.command("e"))
+@app.on_message(Filters.command("delete"))
 def main(client, message):
  x = message.command[1]
  y = message.command[2]
@@ -11,41 +11,4 @@ def main(client, message):
    client.delete_messages(message.chat.id,m)
   except:
    continue
-@app.on_message(Filters.command("b"))
-def main(client, message):
- x = message.command[1]
- y = message.command[2]
- for m in range(int(x),int(y)):
-  try:
-   client.delete_messages(message.chat.id,m)
-  except:
-   continue
-@app.on_message(Filters.command("a"))
-def main(client, message):
- x = message.command[1]
- y = message.command[2]
- for m in range(int(x),int(y)):
-  try:
-   client.delete_messages(message.chat.id,m)
-  except:
-   continue
-@app.on_message(Filters.command("c"))
-def main(client, message):
- x = message.command[1]
- y = message.command[2]
- for m in range(int(x),int(y)):
-  try:
-   client.delete_messages(message.chat.id,m)
-  except:
-   continue
-@app.on_message(Filters.command("d"))
-def main(client, message):
- x = message.command[1]
- y = message.command[2]
- for m in range(int(x),int(y)):
-  try:
-   client.delete_messages(message.chat.id,m)
-  except:
-   continue
-
 app.run()
